507a96ba7bed0cbaaebd83f2c5ebfc7b6f56de2f
Muhammed Fatih Bulut <mfbulut@us.ibm.com>
2019-12-31 12:14:58 -0500

    added react

---
+++ footer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const f_e = React.createElement;
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    return f_e("footer", {
+      className: "footer"
+    }, f_e("div", {
+      className: 'container'
+    }, f_e("p", {
+      className: "text-muted"
+    }, "Last Updated 31 Dec 2019")));
+  }
+}
+
+const footerContainer = document.querySelector('#footer_container');
+ReactDOM.render(e(Footer), footerContainer);
---
+++ navigation.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const e = React.createElement;
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+
+    var goBack = "";
+    if (window.location.href.includes("is690") || window.location.href.includes("CSCI")) {
+      goBack = "../";
+    }
+
+    console.log(goBack);
+
+    return e("nav", {
+      className: "navbar navbar-default navbar-fixed-top"
+    }, e("div", {
+      className: "container"
+    }, e("div", {
+      className: "navbar-header"
+    }, e("button", {
+      type: "button",
+      className: "navbar-toggle collapsed",
+      "data-toggle": "collapse",
+      "data-target": "#navbar",
+      "aria-expanded": "false",
+      "aria-controls": "navbar"
+    }, e("span", {
+      className: "sr-only"
+    }, "Toggle navigation"), e("span", {
+      className: "icon-bar"
+    }), e("span", {
+      className: "icon-bar"
+    }), e("span", {
+      className: "icon-bar"
+    })), e("a", {
+      className: "navbar-brand",
+      href: goBack + "index.html"
+    }, "Fatih Bulut")), e("div", {
+      id: "navbar",
+      className: "collapse navbar-collapse"
+    }, e("ul", {
+      className: "nav navbar-nav"
+    }, e("li", {
+      className: "active"
+    }, e("a", {
+      href: goBack + "index.html"
+    }, "About")), e("li", null, e("a", {
+      target: "_blank",
+      href: "https://medium.com/@dataturka"
+    }, "Blog")), e("li", {
+      className: "dropdown"
+    }, e("a", {
+      href: "#",
+      className: "dropdown-toggle",
+      "data-toggle": "dropdown",
+      role: "button",
+      "aria-haspopup": "true",
+      "aria-expanded": "false"
+    }, "Teaching ", e("span", {
+      className: "caret"
+    })), e("ul", {
+      className: "dropdown-menu"
+    }, e("li", null, e("a", {
+      href: goBack + "CSCI-GA.2262-%E2%80%8B001/index.html"
+    }, "CSCI-GA.2262-001 Spring/20")), e("li", null, e("a", {
+      href: goBack + "is690q/index.html"
+    }, "IS690Q Spring/16"))))))));
+  }
+}
+
+const domContainer = document.querySelector('#nav_container');
+ReactDOM.render(e(Navigation), domContainer);
