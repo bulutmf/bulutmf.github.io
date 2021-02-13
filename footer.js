3f050e68edf204c5acbe303ac547ea5c7a1cde24
Muhammed Fatih Bulut <mfbulut@us.ibm.com>
2021-02-13 09:07:18 -0500

    update feb

--- footer.js
+++ footer.js
@@ -15,7 +15,7 @@ class Footer extends React.Component {
       className: 'container'
     }, f_e("p", {
       className: "text-muted"
-    }, "Last Updated Jan 30, 2021")));
+    }, "Last Updated Feb 13, 2021")));
   }
 }
 
