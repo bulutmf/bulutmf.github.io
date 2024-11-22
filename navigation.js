279df56ad5e3e38248490eab2a2ae7ed519cb7f6
Fatih Bulut <mbulut@microsoft.com>
2024-11-22 09:38:20 -0500

    changed nav

--- navigation.js
+++ navigation.js
@@ -41,7 +41,7 @@ class Navigation extends React.Component {
     })), e("a", {
       className: "navbar-brand",
       href: goBack + "index.html"
-    }, "Fatih Bulut")), e("div", {
+    }, "Muhammed Fatih Bulut")), e("div", {
       id: "navbar",
       className: "collapse navbar-collapse"
     }, e("ul", {
