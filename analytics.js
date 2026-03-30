fbf346a5ea5f06c577d914df154da4789191d3c0
Fatih Bulut <bulutmf@gmail.com>
2026-03-30 10:42:52 -0400

    counts

---
+++ analytics.js
@@ -0,0 +1,12 @@
+(function() {
+  var id = 'G-033H77CV7J';
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + id;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', id);
+})();
